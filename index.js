75c16880711b22aa559685c6dca1319d12ffad76
HomeLanderz <86560837+HomeLanderz@users.noreply.github.com>
2022-10-11 10:29:29 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"},
+]
+
+/* console.log(record[0].result); */
+
+function superbowlWin(array) {
+    const result = array.find(element => element.result === "W") 
+    if (result) {
+        return result.year
+    } else {
+        return undefined
+    }
+
+}
+
